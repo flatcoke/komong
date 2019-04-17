af6e367d151563469e0c3f5484f92ea72f0b2591
flatcoke <flatcoke89@gmail.com>
2019-04-17 17:54:32 +0900

    exmaple
    fix

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -10,7 +10,16 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     email = serializers.EmailField(required=True, validators=[])
     password = serializers.CharField(write_only=True)
     is_staff = serializers.BooleanField(read_only=True)
+    email_without_domain = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'is_staff', 'password')
+        fields = ('id', 'username', 'email', 'is_staff', 'password',
+                  'email_without_domain', 'is_kmong_email',)
+
+    @staticmethod
+    def get_email_without_domain(obj):
+        try:
+            return obj.email.split('@')[0]
+        except AttributeError:
+            return ''
